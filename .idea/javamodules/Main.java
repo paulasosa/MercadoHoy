b08cfe5a2220fedcdf4593dd51b800d60ac0fc19
Paula Sosa Hernandez <paulasosahdez@gmail.com>
2026-01-29 23:31:47 -0500

    First steps with Java

---
+++ .idea/javamodules/Main.java
@@ -0,0 +1,9 @@
+package javamodules;
+
+public class Main {
+    public static void main (String[] args){
+        String myString = "aqui funciona";
+        System.out.println(myString);
+    }
+
+}
